212294bee0489edbb049b57aae4102d702354e2a
Ashley Allen <ashleytjallen@gmail.com>
2021-09-03 21:16:03 +0100

    Extend sf::Vector2<T> with utility functions (but don't use it yet)

--- src/util/vector.h
+++ src/util/vector.h
@@ -62,4 +62,43 @@ std::ostream& operator<<(std::ostream& os, const sf::Vector2<T>& vector) {
     return os << "(" << vector.x << ", " << vector.y << ")";
 }
 
+// Implemented for potential future use but as of now unused due to fears of memory overheads since sf::Vector2<T> is
+// used a lot
+template <typename T>
+class Vector2 : public sf::Vector2<T> {
+   public:
+    using sf::Vector2<T>::Vector2;
+    Vector2(const sf::Vector2<T>& parent);
+
+    float length() const;
+    Vector2<T> normalize() const;
+    float dot(const Vector2<T>& other) const;
+    float angleBetween(const Vector2<T>& other) const;
+};
+
+typedef Vector2<float> Vector2f;
+
+template <typename T>
+inline Vector2<T>::Vector2(const sf::Vector2<T>& parent) : sf::Vector2<T>(parent) {}
+
+template <typename T>
+inline float Vector2<T>::length() const {
+    return util::length(*this);
+}
+
+template <typename T>
+inline Vector2<T> Vector2<T>::normalize() const {
+    return util::normalize(*this);
+}
+
+template <typename T>
+inline float Vector2<T>::dot(const Vector2<T>& other) const {
+    return util::dot(*this, other);
+}
+
+template <typename T>
+inline float Vector2<T>::angleBetween(const Vector2<T>& other) const {
+    return util::angleBetween(*this, other);
+}
+
 }  // namespace util
